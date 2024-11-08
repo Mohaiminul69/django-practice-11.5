d601d630243391b483c45ede009507b4df48af5b
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-08 18:42:16 +0600

    practice done.

--- template_practice/template_practice/settings.py
+++ template_practice/template_practice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "meal_app",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "template_practice.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
